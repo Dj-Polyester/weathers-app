07fe5023f991ed7e30b8cdb33748aead4f686435
Polyester <anonymous_571@outlook.com>
2020-08-06 22:16:44 +0300

    mv files to root

--- weathers-app-js/public/js/loclms.js
+++
@@ -1,8 +0,0 @@
-const access_key =
-  "pk.eyJ1IjoiaGFrYW5kZW1pciIsImEiOiJja2RjdmF4ZWYxbjdwMnlrNnJnOHQ2bXc1In0.LH2YojLbujr0d33Rv_jguQ";
-const limit = 1;
-
-module.exports = {
-  access_key: access_key,
-  limit: limit,
-};
